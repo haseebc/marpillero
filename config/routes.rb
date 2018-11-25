3f4e50ece58e1e23c488a66129dd7e4695fbe2ea
Haseeb Chaudhary <haseebchaudhary@gmail.com>
2018-11-25 19:07:40 +0000

    structure added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'pages/about', to: 'pages#about'
+  get 'pages/contact', to: 'pages#contact'
+  get 'pages/blog', to: 'pages#blog'
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'blog', to: 'pages#blog'
+
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
