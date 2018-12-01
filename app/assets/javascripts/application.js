6caf8f2cbe90f3ebfac4ab63207aac03ca75b4ea
Haseeb Chaudhary <haseebchaudhary@gmail.com>
2018-12-01 21:01:55 +0000

    home page styled a bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
+
 //= require rails-ujs
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+    $( "a.scroll" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
+});
