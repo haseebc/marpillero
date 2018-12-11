6326b1b30091123e0b4b0e8a8f72f729b8888cc1
Haseeb Chaudhary <haseebchaudhary@gmail.com>
2018-12-11 21:33:22 +0000

    blog started

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
-  skip_before_action :authenticate_user!, only: %i[home blog about contact]
+  skip_before_action :authenticate_user!, only: %i[home blog about contact index]
 
+  def index
+  end
+  
   def home
   end
 
